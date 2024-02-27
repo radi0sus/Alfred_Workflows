b86198aeb4f3b9eb87f558a57ee0f491d30f86f3
Sebastian Dechert <p212121@googlemail.com>
2024-02-27 19:52:15 +0100

    safety information from PubChem

--- pubchem_retrieve/checkprop_req.sh
+++ pubchem_retrieve/checkprop_req.sh
@@ -4,6 +4,7 @@ comp=$compound
 # delete leading and trailing whitespaces
 comp=$(echo "$comp" | sed 's/^[ ]*//;s/[ ]*$//')
 
+
 # Properties to retrieve from PubChem
 prop="Title,MolecularFormula,MolecularWeight,ExactMass,InChIKey,IsomericSMILES,IUPACName"
 
@@ -14,19 +15,19 @@ regexp="[A-Z]{14}-[A-Z]{10}-[A-Z]{1}"
 if [[ $comp =~ $regexp ]]; then
     # for InChIKey
     resp=$(curl -s -X POST \
-      -H "Content-Type: application/x-www-form-urlencoded" \
+       -H "Content-Type: application/x-www-form-urlencoded" \
       --data-urlencode "inchikey=$comp" \
-      -w "%{http_code}" https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/inchikey/cids/TXT | \
+       -w "%{http_code}" https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/inchikey/cids/TXT | \
       # in case that there are more than cids, take the first one; also submit HTTP status code 
-          awk 'NR==1 ; END{print}' | tr -d '\n')
+        awk 'NR==1 ; END{print}' | tr -d '\n')
 else
     # for name
     resp=$(curl -s -X POST \
-      -H "Content-Type: application/x-www-form-urlencoded" \
+       -H "Content-Type: application/x-www-form-urlencoded" \
       --data-urlencode "name=$comp" \
-      -w "%{http_code}" https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/cids/TXT | \
-      # in case that there are more than cids, take the first one; also submit HTTP status code 
-          awk 'NR==1 ; END{print}' | tr -d '\n')
+       -w "%{http_code}" https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/cids/TXT | \
+      # in case that there are more than one cid, take the first one; also submit HTTP status code 
+        awk 'NR==1 ; END{print}' | tr -d '\n')
 fi
 
 # get HTTP status code
@@ -71,20 +72,20 @@ else
           | .subtitle = "Name" | .arg = .PropertyTable.Properties[0].Title 
           | .icon.path= "./images/temp.png" | del(.PropertyTable)')
     # get IUPAC Name
-        inam=$(echo "$req" \
-              | jq 'if has("PropertyTable") and (.PropertyTable.Properties[0].IUPACName 
-              | length > 0) then 
-                .title = .PropertyTable.Properties[0].IUPACName 
-              | .subtitle = "IUPAC Name" 
-              | .arg = .PropertyTable.Properties[0].IUPACName 
-              | .icon.path= "./images/temp.png" 
-              | del(.PropertyTable)
-          else
-                .title = "not found"
-              | .subtitle = "IUPAC Name" 
-              | .arg = "not found" 
-              | .icon.path= "./images/temp.png" 
-              | del(.PropertyTable)
+    inam=$(echo "$req" \
+          | jq 'if has("PropertyTable") and (.PropertyTable.Properties[0].IUPACName 
+          | length > 0) then 
+            .title = .PropertyTable.Properties[0].IUPACName 
+          | .subtitle = "IUPAC Name" 
+          | .arg = .PropertyTable.Properties[0].IUPACName 
+          | .icon.path= "./images/temp.png" 
+          | del(.PropertyTable)
+    else
+          .title = "not found"
+          | .subtitle = "IUPAC Name" 
+          | .arg = "not found" 
+          | .icon.path= "./images/temp.png" 
+          | del(.PropertyTable)
           end')
     # get InChI
     inci=$(echo "$req" \
@@ -111,7 +112,6 @@ else
     CAS=$(curl -s https://pubchem.ncbi.nlm.nih.gov/rest/pug_view/data/compound/$cid/JSON?heading=CAS \
           | jq '.Record.Section[] 
           | .Section[] | .Section[] 
-          | select(.TOCHeading == "CAS") 
           | .Information[0].Value.StringWithMarkup[].String' \
           | tr -d '"')
     # CAS for Alfred
@@ -122,7 +122,75 @@ else
           | .subtitle = "CAS Number" 
           | .arg = $cas 
           | .icon.path= "./images/temp.png"')
-
+          
+    # get safety data with 'pug_view' 
+    sfty=$(curl -s https://pubchem.ncbi.nlm.nih.gov/rest/pug_view/data/compound/$cid/JSON?heading=Safety+and+Hazards)
+    
+    # pictogram text
+    pict=$(echo "$sfty" \
+          | jq '.Record.Section[0] 
+          | .Section[0] | .Section[0] 
+          | .Information[0].Value.StringWithMarkup[].Markup[].Extra')
+    if [ -z "$pict" ]; then
+        pict=$(echo "not found")
+    else
+        pict=$(echo $pict | sed 's/ "/, /g' |  tr -d '"')
+    fi
+    # pictogram text for Alfred
+    pict=$(jq -n --arg pict "$pict" '.title = $pict 
+          | .subtitle = "Pictogram(s) Text" 
+          | .arg = $pict 
+          | .icon.path= "./images/temp.png"')
+    
+    # signal word
+    sign=$(echo "$sfty" \
+          | jq '.Record.Section[0] 
+          | .Section[0] | .Section[0] 
+          | .Information[1].Value.StringWithMarkup[].String')
+    if [ -z "$sign" ]; then
+        sign=$(echo "not found")
+    else
+        sign=$(echo "$sign" | tr -d '"')
+    fi
+    # signal word for Alfred
+    sign=$(jq -n --arg sign "$sign" '.title = $sign 
+          | .subtitle = "Signal Word" 
+          | .arg = $sign 
+          | .icon.path= "./images/temp.png"')
+    
+    # GHS hazard statement codes
+    hsta=$(echo "$sfty" \
+          | jq '.Record.Section[0] 
+          | .Section[0] | .Section[0] 
+          | .Information[2].Value.StringWithMarkup[].String')
+    if [ -z "$hsta" ]; then
+        hsta=$(echo "not found")
+    else
+        hsta=$(echo "$hsta" | grep -o 'H[0-9][0-9][0-9]' \
+               | tr '\n' ',' | sed 's/,/, /g' | sed 's/, $//')
+    fi
+    # GHS hazard statement codes for Alfred
+    hsta=$(jq -n --arg hsta "$hsta" '.title = $hsta 
+          | .subtitle = "GHS Hazard Statement Codes" 
+          | .arg = $hsta 
+          | .icon.path= "./images/temp.png"')
+    
+    # GHS precautionary statement codes
+    psta=$(echo "$sfty" \
+           | jq '.Record.Section[0] 
+           | .Section[0] | .Section[0] 
+           | .Information[3].Value.StringWithMarkup[0].String')
+    if [ "$psta" == null ]; then
+        psta=$(echo "not found")
+    else
+        psta=$(echo $psta | sed 's/"//g; s/and//g')
+    fi
+    # GHS precautionary statement codes for Alfred
+    psta=$(jq -n --arg psta "$psta" '.title = $psta 
+          | .subtitle = "GHS Precautionary Statement Codes" 
+          | .arg = $psta 
+          | .icon.path= "./images/temp.png"')
+    
     # all properties for clipboard
     cname=$(echo "$name" | jq '.arg')
     cinam=$(echo "$inam" | jq '.arg')
@@ -133,15 +201,21 @@ else
     cismi=$(echo "$ismi" | jq '.arg')
     ccasn=$(echo "$casn" | jq '.arg')
     cowww=$(echo "$owww" | jq '.arg')
+    cpict=$(echo "$pict" | jq '.arg')
+    chsta=$(echo "$hsta" | jq '.arg')
+    cpsta=$(echo "$psta" | jq '.arg')
+    csign=$(echo "$sign" | jq '.arg')
     allp=$(jq -n --arg name "$cname" --arg inam "$cinam" --arg molf "$cmolf" \
                  --arg molw "$cmolw" --arg emas "$cemas" --arg inci "$cinci" \
                  --arg ismi "$cismi" --arg casn "$ccasn" --arg owww "$cowww" \
+                 --arg pict "$cpict" --arg hsta "$chsta" --arg psta "$cpsta" \
+                 --arg sign "$csign" \
                  '.title = "Copy all of the above to the clipboard" 
                 | .subtitle = $name 
-                | .arg = ["allprop",$name,$inam,$molf,$molw,$emas,$inci,$ismi,$casn,$owww]
+                | .arg = ["allprop",$name,$inam,$molf,$molw,$emas,$inci,$ismi,$casn,$owww,$pict,$sign,$hsta,$psta]
                 | .icon.path= "./images/temp.png"') 
     # build list of items
-    new_req="{\"items\": [ $name, $inam, $molf, $molw, $emas, $inci, $ismi, $casn, $owww, $allp]}"
+    new_req="{\"items\": [ $name, $inam, $molf, $molw, $emas, $inci, $ismi, $casn, $owww, $pict, $sign, $hsta, $psta, $allp]}"
 fi
 
 # show in Alfred
